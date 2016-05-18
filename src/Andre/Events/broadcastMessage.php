f6ace9fe3d0afe8e5346007bc03c2ea2b5417bdb
Andre <andrethegamer911@gmail.com>
2016-05-18 13:06:46 -0400

    Broadcasting done!

---
+++ src/Andre/Events/broadcastMessage.php
@@ -0,0 +1,19 @@
+<?php
+namespace RagingPE\Events;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Server;
+use pocketmine\plugin\Plugin;
+
+class Task extends PluginTask{
+     public function __construct(Plugin $owner){
+          parent::__construct($owner);
+          $this->plugin = $owner;
+     }
+     public function onRun($currentTick){
+          $broadcast = $this->plugin->broadcast;
+          $message = $broadcast->get("Messages");
+          $messages = $message[array_rand($message)];
+          Server::getInstance()->broadcastMessage($messages);
+     }
+}
