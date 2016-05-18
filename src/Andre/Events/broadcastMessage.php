058533f64f3911d7dea08f089cad6a8d83fa0701
Andre <andrethegamer911@gmail.com>
2016-05-18 14:30:47 -0400

    Class issue fixed

--- src/Andre/Events/broadcastMessage.php
+++ src/Andre/Events/broadcastMessage.php
@@ -1,19 +1,19 @@
 <?php
-namespace HubPE\Events;
+namespace Andre\Events;
 
 use pocketmine\scheduler\PluginTask;
 use pocketmine\Server;
 use pocketmine\plugin\Plugin;
 
-class Task extends PluginTask{
+class broadcastMessage extends PluginTask{
      public function __construct(Plugin $owner){
           parent::__construct($owner);
           $this->plugin = $owner;
      }
      public function onRun($currentTick){
-          $broadcast = $this->plugin->this->broadcast;
-          $message = $broadcast->get("Messages");
-          $messages = $message[array_rand($message)];
-          Server::getInstance()->broadcastMessage($messages);
+         // $broadcast = $this->plugin->this->broadcast;
+         // $message = $broadcast->get("Messages");                    *removed for not*
+        //  $messages = $message[array_rand($message)];
+          Server::getInstance()->broadcastMessage("Default message");
      }
 }
