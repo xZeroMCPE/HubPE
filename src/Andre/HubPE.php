f480bfdbba1585932ac4967c3b9faa1216ffa6d2
Andre <andrethegamer911@gmail.com>
2016-05-18 13:03:40 -0400

    :)

--- src/Andre/HubPE.php
+++ src/Andre/HubPE.php
@@ -24,6 +24,7 @@ class Core extends PluginBase implements Listener {
             $this->saveDefaultConfig();
             $this->reloadConfig();
             $this->config = new Config($this->getDataFolder()) . "config.yml"), Config::YAML));
+            $this->getServer()->getScheduler()->scheduleRepeatingTask(new Events\broadcastMessage($this), 400);
             $this->getLogger()->info("HubPE has been enabled!");
      }
      public function onBlockBreak(BlockBreakEvent $event){
