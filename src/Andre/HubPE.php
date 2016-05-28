c97cc8cc55dff93fd775a401d05638bb8aa03662
Ankit <ankitmaharjankath@gmail.com>
2016-05-28 13:07:51 +0545

    Code Clean up again

--- src/Andre/HubPE.php
+++ src/Andre/HubPE.php
@@ -27,52 +27,52 @@ class HubPE extends PluginBase implements Listener {
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 		$this->saveResource("BroadcastMessage.yml");
 		$this->saveDefaultConfig();
-        $this->config = (new Config($this->getDataFolder()."config.yml", Config::YAML));
+		$this->config = (new Config($this->getDataFolder()."config.yml", Config::YAML));
 		$this->broadcast = (new Config($this->getDataFolder()."BroadcastMessage.yml", Config::YAML));
-        $this->getServer()->getScheduler()->scheduleRepeatingTask(new Events\broadcastMessage($this), 400);
-        $this->getLogger()->info("HubPE has been enabled!");
-     }
-    public function onBlockBreak(BlockBreakEvent $event){  
-        $player = $event->getPlayer();
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new Events\broadcastMessage($this), 400);
+		$this->getLogger()->info("HubPE has been enabled!");
+	}
+	public function onBlockBreak(BlockBreakEvent $event){  
+		$player = $event->getPlayer();
 		if(!$player->hasPermission("HubPE.break")){
 			$event->setCancelled();
 			$player->sendPopup("You are not allowed to break blocks here.");
-        }
-    }
-    public function onBlockPlace(BlockPlaceEvent $event){  
+		}
+	}
+	public function onBlockPlace(BlockPlaceEvent $event){  
 		$player = $event->getPlayer();
-        if(!$player->hasPermission("HubPE.place")){
+		if(!$player->hasPermission("HubPE.place")){
 			$event->setCancelled();
 			$player->sendPopup("You are not allowed to place blocks here.");
-        }
-    }
-    public function onKick(PlayerKickEvent $event){
-        if($event->getReason() === "disconnectionScreen.serverFull"){
-			if($event->getPlayer()->hasPermission("HubPE.full")){
-                $event->setCancelled(true);
-            }
-        }
-    }
-    public function onJoin(PlayerJoinEvent $e){
-        $player = $e->getPlayer();
-        $Join_Message = $this->getConfig()->get("Join_Message");
-        $player->sendMessage("$Join_Message");
-        $player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-    }
-    public function onChat(PlayerChatEvent $event){
-        $message = $event->getMessage();
-        $Disable_Commands = $this->getConfig()->get("Disable_Commands");
-        if($message === $Disable_Commands){
-            $event->setCancelled();
-        }
+		}
 	}
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-        switch ($command){
-            case 'HubPE':
-           	if(empty($args[0])){
+	public function onKick(PlayerKickEvent $event){
+		if($event->getReason() === "disconnectionScreen.serverFull"){
+			if($event->getPlayer()->hasPermission("HubPE.full")){
+				$event->setCancelled(true);
+			}
+		}
+	}
+	public function onJoin(PlayerJoinEvent $e){
+		$player = $e->getPlayer();
+		$Join_Message = $this->getConfig()->get("Join_Message");
+		$player->sendMessage("$Join_Message");
+		$player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+	}
+	public function onChat(PlayerChatEvent $event){
+		$message = $event->getMessage();
+		$Disable_Commands = $this->getConfig()->get("Disable_Commands");
+		if($message === $Disable_Commands){
+			$event->setCancelled();
+		}
+	}
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+		switch ($command){
+			case 'HubPE':
+			if(empty($args[0])){
 				$sender->sendMessage("/HubPE setlobby - Sets the main world spawn\n/HubPE - Shows a list of HubPE commands ");
-            } else {
-                if($args[0] == "setlobby"){
+			} else {
+				if($args[0] == "setlobby"){
 					if($sender instanceof Player){
 						$x = $sender->getX();
 						$y = $sender->gety();
@@ -82,31 +82,35 @@ class HubPE extends PluginBase implements Listener {
 						$this->config->set("LobbyZ", "$x");
 						$sender->sendMessage("Lobby spawn has been set to your position");
 					} else {
-                        $sender->sendMessage("Run this command in game");
-                    }
+						$sender->sendMessage("Run this command in game");
+					}
 				}
-                if($args[0] == "lobby"){
+				if($args[0] == "lobby"){
 					if($sender instanceof Player){
 						$sender->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
 					} else {
-                        $sender->sendMessage("Run this command in game");
-                    }
+						$sender->sendMessage("Run this command in game");
+					}
 				}
 				if($args[0] == "fly"){
 					if($sender instanceof Player){
 						if($sender->hasPermission("HubPE.fly")){
 							if(isset($this->flight[$sender->getName()])){
 								$sender->setAllowFlight(false);
+								$sender->sendMessage("You have disabled flight mode");
+								unset($this->flight[$sender->getName()]);
 							} else {
 								$sender->setAllowFlight(true);
+								$sender->sendMessage("You have enabled flight mode");
+								$this->flight[$sender->getName()] = $sender->getName();
 							}
 						}
 					} else {
-                        $sender->sendMessage("Run this command in game");
-                    }
+						$sender->sendMessage("Run this command in game");
+					}
 				} else {
 					$sender->sendMessage("Missing Parameter(s)");
 				}
-            }
+			}
 		}
 	}
