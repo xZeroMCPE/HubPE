bf568de8b35e6991cb3fe327a7e1f98b4ef10143
Ankit <ankitmaharjankath@gmail.com>
2016-05-28 13:11:48 +0545

    Code cleanup

--- src/Andre/Events/broadcastMessage.php
+++ src/Andre/Events/broadcastMessage.php
@@ -6,16 +6,16 @@ use pocketmine\Server;
 use pocketmine\plugin\Plugin;
 
 class broadcastMessage extends PluginTask{
-     public function __construct(Plugin $owner){
-          parent::__construct($owner);
-          $this->plugin = $owner;
-     }
-     public function onRun($currentTick){
-         // $broadcast = $this->plugin->this->broadcast;
-         // $message = $broadcast->get("Messages");                    *removed for now*
-        //  $messages = $message[array_rand($message)];
-        $messages = $this->plugin->broadcast->get("Messages");
-        $randommessage = $messages[array_rand($messages)]; 
-          Server::getInstance()->broadcastMessage("$randommessage");
-     }
+	public function __construct(Plugin $owner){
+		parent::__construct($owner);
+		$this->plugin = $owner;
+	}
+	public function onRun($currentTick){
+		// $broadcast = $this->plugin->this->broadcast;
+		// $message = $broadcast->get("Messages");                    *removed for now*
+		//  $messages = $message[array_rand($message)];
+		$messages = $this->plugin->broadcast->get("Messages");
+		$randommessage = $messages[array_rand($messages)]; 
+		Server::getInstance()->broadcastMessage("$randommessage");
+	}
 }
